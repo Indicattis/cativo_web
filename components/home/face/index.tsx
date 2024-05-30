7fa6c68ea97de9b1d8580b005afe3468dfcd5a2a
Indicattis <joao.staehler@gmail.com>
2024-05-29 22:53:42 -0300

    FE | homepage | Update

--- components/home/face/index.tsx
+++ components/home/face/index.tsx
@@ -5,7 +5,7 @@ import SliderComponent from "@/components/inc/slider";
 import { menu_array } from "@/data/json/menu";
 
 export default function FaceComponent() {
-    const [selectedItem, setSelectedItem] = useState<number>(0);
+    const [selectedItem, setSelectedItem] = useState<number>(1);
 
     return (
         <section className="_section min-h-[50vh] max-h-[75vh] z-10 relative overflow-hidden">
