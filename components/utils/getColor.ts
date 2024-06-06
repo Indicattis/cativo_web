eb7ecad9bcdb0073bb520117096e059e2b6308a0
Indicattis <joao.staehler@gmail.com>
2024-06-05 22:15:55 -0300

    FE | visual | Update animations loop 2

---
+++ components/utils/getColor.ts
@@ -0,0 +1,26 @@
+
+
+
+
+export default function getColor(color: string) {
+
+    switch (color) {
+        case "neon_green":
+            return `bg-gradient-to-tr from-neon_green2 to-neon_green` 
+            break;
+        case "neon_red":
+            return `bg-gradient-to-tr from-neon_red to-neon_pink` 
+            break;
+        case "neon_purple":
+            return `bg-gradient-to-tr from-neon_purple to-neon_blue` 
+            break;
+        case "neon_blue":
+            return `bg-gradient-to-tr from-neon_blue to-neon_green` 
+            break;
+        case "neon_pink":
+            return `bg-gradient-to-tr from-neon_pink2 to-neon_pink` 
+            break;
+        default:
+            break;
+    }
+}
