83cbc3123ebd73d73e66554ad0d6ea0caff5745d
Indicattis <joao.staehler@gmail.com>
2024-06-06 16:25:51 -0300

    FE | homepage | video test

---
+++ data/json/sliders/intro.ts
@@ -0,0 +1,32 @@
+import { SliderIntroDTO } from "@/data/types/sliders"
+
+
+const intro_sliders: SliderIntroDTO[] = [
+        {
+            id: 1,
+            exhibition: "Web Marcket",
+            url: "/video/background-animation-loop.mp4"
+        },
+        {
+            id: 2,
+            exhibition: "Social Medias",
+            url: "/img/banners/neon_blue.png"
+        },
+        {
+            id: 3,
+            exhibition: "Gestão de tráfego",
+            url: "/img/banners/neon_green.png"
+        },
+        {
+            id: 4,
+            exhibition: "",
+            url: "/img/banners/neon_purple.png"
+        },
+        {
+            id: 5,
+            exhibition: "",
+            url: "/img/banners/neon_pink.png"
+        },
+]
+
+export {intro_sliders}
---
+++ data/types/sliders.ts
@@ -0,0 +1,8 @@
+
+
+
+export type SliderIntroDTO = {
+    id: number;
+    exhibition: string;
+    url: string;
+}
