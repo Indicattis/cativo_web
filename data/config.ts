c4ed56cf435010bdb09e079e805f37b1c2aab42a
unknown <joao.staehler@gmail.com>
2024-07-25 11:02:43 -0300

    FE | Constante de comunicação com o server atualizada

--- data/config.ts
+++ data/config.ts
@@ -2,5 +2,5 @@
 
 
 
-// export const SERVER_URL = `https://professional-page-api.vercel.app`
-export const SERVER_URL = `http://localhost:3333`
+export const SERVER_URL = `https://professional-page-api.vercel.app`
+// export const SERVER_URL = `http://localhost:3333`
